c85106c5279d7565f260b0f0cc6c7b17b21d191a
pirahawk <piran.tata@gmail.com>
2019-06-17 23:11:59 +0100

    Added ping controller

---
+++ Bog.Api.Web/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bog.Api.Web.Controllers
+{
+    [Route("api/ping")]
+    public class PingController : Controller
+    {
+        [HttpGet()]
+        public IActionResult Ping()
+        {
+            return NoContent();
+        }
+    }
+}
