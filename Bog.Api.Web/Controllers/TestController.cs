a1b2a56bfb9196018433febef58d06c4c5c672f8
pirahawk <piran.tata@gmail.com>
2019-06-17 22:54:09 +0100

    Initial commit

---
+++ Bog.Api.Web/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bog.Api.Web.Controllers
+{
+    [Route("api/test")]
+    public class TestController : Controller
+    {
+        [HttpGet()]
+        public IActionResult Test()
+        {
+            return Ok("This Works");
+        }
+    }
+}
